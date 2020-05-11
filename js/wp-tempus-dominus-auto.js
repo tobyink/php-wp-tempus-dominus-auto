bd7a9230b2ca74ffa389d43f36d5b53a637b494f
Toby Inkster <mail@tobyinkster.co.uk>
2020-05-11 11:56:49 +0100

    add test cases; minor bugfixes; change button to "x" to indicate it can be used to close popover

--- js/wp-tempus-dominus-auto.js
+++ js/wp-tempus-dominus-auto.js
@@ -12,6 +12,13 @@
 				href: "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
 			}).appendTo('head');
 			
+			jQuery.fn.datetimepicker.Constructor.prototype._notifyEvent = function _notifyEvent(e) {
+				if (e.type === jQuery.fn.datetimepicker.Constructor.Event.CHANGE && (e.date && e.date.isSame(e.oldDate) || !e.date && !e.oldDate)) {
+					return;
+				}
+				this._element.trigger(e);
+			};
+			
 			$(function () {
 				$('input[type=date], input[type=time], input[type=datetime], input.tempus-dominus, input.tempus-dominus-time, input.tempus-dominus-date')
 					.not('.no-tempus-dominus')
@@ -36,58 +43,74 @@
 							format = 'YYYY-MM-DD';
 						}
 						
-						var $group = $input.wrap('<div class="input-group date" id="' + id + '" data-target-input="nearest"></div>').parent();
-						$group.append('<div class="input-group-append" data-target="#' + id + '" data-toggle="datetimepicker"><div class="input-group-text"><i class="fa ' + icon + '"></i></div></div>');
-						
 						if ( e.hasAttribute('data-tempus-dominus-format') ) {
 							format = e.getAttribute('data-tempus-dominus-format');
 						}
 						
 						if ( e.hasAttribute('data-tempus-dominus-locale') ) {
-							locale = e.getAttribute('data-tempus-dominus-locale');
+							local2 = e.getAttribute('data-tempus-dominus-locale');
 						}
+						
+						if ( e.hasAttribute('data-tempus-dominus-icon') ) {
+							icon = e.getAttribute('data-tempus-dominus-icon');
+						}
+						
+						var $group = $input.wrap('<div class="input-group date" id="' + id + '" data-target-input="nearest"></div>').parent();
+						$group.append('<div class="input-group-append" data-target="#' + id + '" data-toggle="datetimepicker"><div class="input-group-text"><i class="fa ' + icon + '"></i></div></div>');
 						
 						$input.attr('type', 'text');
 						$group.datetimepicker({ "format": format, "locale": local2 });
 						
+						$group.on('hide.datetimepicker', function () {
+							$group.find('.input-group-text').html('<i class="fa ' + icon + '"></i>');
+						});
+						$group.on('show.datetimepicker', function () {
+							$group.find('.input-group-text').html('<i class="fa fa-close"></i>');
+						});
+						
 						if ( e.hasAttribute('data-tempus-dominus-onclick') ) {
 							$input.click(function () { $group.datetimepicker('toggle'); });
 						}
-
+						
 						if ( e.hasAttribute('data-tempus-dominus-overlay') ) {
 							$group.css({ 'position': 'relative' });
-							$group.append('<span class="tempus-dominus-overlay"></span>');
+							$group.append('<span class="tempus-dominus-overlay text-muted"></span>');
 							var $overlay = $group.find('.tempus-dominus-overlay');
 							var overlay_fmt = e.getAttribute('data-tempus-dominus-overlay');
 							$overlay.css({
 								'position':  'absolute',
 								'top':       '5px',
-								'right':     ( $group.find('.input-group-append').height() + 10 )+'px',   // needs fixing
+								'right':     ( $group.find('.input-group-append').height() + 10 )+'px',
 								'z-index':   5,
 								'font-size': '80%',
-								'color':     '#999',
 							});
 							$input.change(function () {
-								var m = moment( $input.val() );
+								var m = moment( $input.val(), format );
+								console.log("HERE1");
+								console.log(m);
 								$overlay.text( m.format(overlay_fmt) );
 							});
 							$input.keyup(function () {
-								var m = moment( $input.val() );
+								var m = moment( $input.val(), format );
+								console.log("HERE2");
+								console.log(m);
 								$overlay.text( m.format(overlay_fmt) );
 							});
 							$group.on("change.datetimepicker", function (e) {
-								var m = moment( e.date );
+								var m = e.date;
+								console.log("HERE3");
+								console.log(m);
 								$overlay.text( m.format(overlay_fmt) );
 							});
 						}
 						
 						// Put data-tempus-dominus-linked="#id" on the end time
 						if ( $input.attr('data-tempus-dominus-linked') ) {
-							var $linked = $( $input.attr('data-tempus-dominus-linked') );
+							var $linked = $( $input.attr('data-tempus-dominus-linked') ).parents('.input-group');
 							$linked.on("change.datetimepicker", function (e) {
-								$input.datetimepicker('minDate', e.date);
+								$group.datetimepicker('minDate', e.date);
 							});
-							$input.on("change.datetimepicker", function (e) {
+							$group.on("change.datetimepicker", function (e) {
 								$linked.datetimepicker('maxDate', e.date);
 							});
 						}
