e9e6cba6fef69b3e9299f36e2189b4f4453ac47d
Toby Inkster <mail@tobyinkster.co.uk>
2020-09-29 21:55:53 +0100

    add licence comments

--- wp-tempus-dominus-auto.php
+++ wp-tempus-dominus-auto.php
@@ -5,7 +5,26 @@
  * Description: Shim between Bootstrap 4.x, JQuery 3.x, WordPress 5.x, and Tempus Dominus
  * Version: 1.0
  * Author: Toby Inkster
- * Author URI: http://toby.ink/
+ * Author URI: https://toby.ink/
+ * License: GPL v3 or later
+ * License URL: https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
+/* wp-tempus-dominus-auto
+ * Copyright (c) 2020 Toby Inkster
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 add_action( 'wp_enqueue_scripts', function () {
